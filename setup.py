b63f1eb8bb9f6715e6ad88b5621ef9e7732ccf84
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2019-08-22 21:10:28 +0900

    gloss improved

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ URL="https://github.com/KoichiYasuoka/UD-Kanbun"
 
 setuptools.setup(
   name="udkanbun",
-  version="0.9.2",
+  version="0.9.3",
   description="Tokenizer POS-tagger and Dependency-parser for Classical Chinese",
   long_description=long_description,
   long_description_content_type="text/markdown",
