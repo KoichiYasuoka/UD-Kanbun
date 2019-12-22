3f4b6fc4450629497b5a8f6c3278e0c69a76f5a5
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2019-12-22 12:02:31 +0900

    version 1.5.7

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except:
 
 setuptools.setup(
   name="udkanbun",
-  version="1.5.6",
+  version="1.5.7",
   description="Tokenizer POS-tagger and Dependency-parser for Classical Chinese",
   long_description=long_description,
   long_description_content_type="text/markdown",
