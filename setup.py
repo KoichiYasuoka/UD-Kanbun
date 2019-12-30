8e6b27606e335169d228436e52f6ad160cc34d0a
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2019-12-30 19:52:23 +0900

    version 1.6.8

--- setup.py
+++ setup.py
@@ -1,29 +1,33 @@
 import setuptools
 import subprocess
+import platform
 import os
 
 with open("README.md","r",encoding="utf-8") as r:
   long_description=r.read()
 URL="https://github.com/KoichiYasuoka/UD-Kanbun"
 
-try:
-  d=subprocess.check_output(["mecab-config","--libs-only-L"])
-  useFugashi=True
-except:
-  useFugashi=(os.name=="nt")
-if useFugashi:
-  install_requires=["ufal.udpipe>=1.2.0.3","fugashi>=0.1.8"]
+pl=platform.platform()
+if pl.startswith("CYGWIN"):
+  install_requires=["ufal.udpipe>=1.2.0","mecab-cygwin"]
 else:
-  import platform
-  from pkg_resources import get_distribution
-  if platform.platform().startswith("Linux") and int(get_distribution("pip").version.split(".")[0])<19:
-    install_requires=["ufal.udpipe>=1.2.0","mecab-python3==0.996.2"]
+  try:
+    d=subprocess.check_output(["mecab-config","--libs-only-L"])
+    useFugashi=True
+  except:
+    useFugashi=(os.name=="nt")
+  if useFugashi:
+    install_requires=["ufal.udpipe>=1.2.0.3","fugashi>=0.1.8"]
   else:
-    install_requires=["ufal.udpipe>=1.2.0","mecab-python3>=0.996.3"]
+    from pkg_resources import get_distribution
+    if pl.startswith("Linux") and int(get_distribution("pip").version.split(".")[0])<19:
+      install_requires=["ufal.udpipe>=1.2.0","mecab-python3==0.996.2"]
+    else:
+      install_requires=["ufal.udpipe>=1.2.0","mecab-python3>=0.996.3"]
 
 setuptools.setup(
   name="udkanbun",
-  version="1.6.7",
+  version="1.6.8",
   description="Tokenizer POS-tagger and Dependency-parser for Classical Chinese",
   long_description=long_description,
   long_description_content_type="text/markdown",
