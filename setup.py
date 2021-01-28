8d3e8a7dca289b6a484435d2cbd3b118fa1ea499
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2021-01-28 09:05:40 +0900

    version 2.7.1

--- setup.py
+++ setup.py
@@ -18,13 +18,13 @@ else:
     import os
     useFugashi&=(os.name=="nt")
   if useFugashi:
-    install_requires=["ufal.udpipe>=1.2.0.3","fugashi>=1.0.5","deplacy>=1.8.9"]
+    install_requires=["ufal.udpipe>=1.2.0.3","fugashi>=1.1.0","deplacy>=1.8.9"]
   else:
     install_requires=["ufal.udpipe>=1.2.0","mecab-python3>=0.996.5","deplacy>=1.8.9"]
 
 setuptools.setup(
   name="udkanbun",
-  version="2.7.0",
+  version="2.7.1",
   description="Tokenizer POS-tagger and Dependency-parser for Classical Chinese",
   long_description=long_description,
   long_description_content_type="text/markdown",
