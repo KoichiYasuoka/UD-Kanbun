30f7125244c6f6819095405cfc967be18f220eec
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2021-02-10 16:12:24 +0900

    version 2.7.4

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ URL="https://github.com/KoichiYasuoka/UD-Kanbun"
 
 pl=platform.platform()
 if pl.startswith("CYGWIN"):
-  install_requires=["ufal.udpipe>=1.2.0","mecab-cygwin>=0.5.0","deplacy>=1.8.9"]
+  install_requires=["ufal.udpipe>=1.2.0","mecab-cygwin>=0.5.0","deplacy>=1.9.1"]
 else:
   import sys
   useFugashi=(sys.version_info.major==3)and(sys.version_info.minor>4)
@@ -18,13 +18,13 @@ else:
     import os
     useFugashi&=(os.name=="nt")
   if useFugashi:
-    install_requires=["ufal.udpipe>=1.2.0.3","fugashi>=1.1.0","deplacy>=1.8.9"]
+    install_requires=["ufal.udpipe>=1.2.0.3","fugashi>=1.1.0","deplacy>=1.9.1"]
   else:
-    install_requires=["ufal.udpipe>=1.2.0","mecab-python3>=0.996.5","deplacy>=1.8.9"]
+    install_requires=["ufal.udpipe>=1.2.0","mecab-python3>=0.996.5","deplacy>=1.9.1"]
 
 setuptools.setup(
   name="udkanbun",
-  version="2.7.3",
+  version="2.7.4",
   description="Tokenizer POS-tagger and Dependency-parser for Classical Chinese",
   long_description=long_description,
   long_description_content_type="text/markdown",
