9eec1d1d1ce427821f36d0b829b622ec12933f1d
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2021-04-15 18:54:09 +0900

    version 3.0.8

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ else:
 
 setuptools.setup(
   name="udkanbun",
-  version="3.0.7",
+  version="3.0.8",
   description="Tokenizer POS-tagger and Dependency-parser for Classical Chinese",
   long_description=long_description,
   long_description_content_type="text/markdown",
